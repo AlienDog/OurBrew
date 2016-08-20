3cb43f5d01d2576f959c6c66d5e9e7d6d316c946
coc4tm <corentin.bzh@outlook.com>
2016-08-20 14:33:03 +0400

    Add files via upload

---
+++ public_html/vita/actdat/actdat.php
@@ -0,0 +1,39 @@
+<?php
+ 
+ $data = array(
+         "loginid" => $_POST['loginid'], /* email */
+         "password" => $_POST['password'], /* password */
+         "consoleid" => $_POST['consoleid'], /* idps */
+         "platform" => "psp2", /* for vita, don't modify, possible parameters psp,ps3,psp2.ps4 is unknown */
+         "acttype" => "4", /* for vita, don't modify */
+ );
+ $custom_headers = array(
+         "X-I-5-DRM-Version: 1.0",
+ );
+ 
+ $ch = curl_init();
+ 
+ curl_setopt($ch, CURLOPT_URL, "https://commerce.np.ac.playstation.net/cap.m");
+ curl_setopt($ch, CURLOPT_USERAGENT, "Legium pro Britania");
+ curl_setopt($ch, CURLOPT_HTTPHEADER, $custom_headers);
+ 
+ curl_setopt($ch, CURLOPT_POST, 1);
+ curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+ 
+ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+ 
+ //curl_setopt($ch, CURLOPT_HEADER, 1);
+ //curl_setopt($ch, CURLOPT_CERTINFO, 1);
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+ curl_setopt($ch, CURLOPT_VERBOSE, 1);
+ 
+ $response = curl_exec($ch);
+ 
+ curl_close($ch);
+ 
+ $fp = fopen("act.dat", "wb");
+ fwrite($fp, $response);
+ fclose($fp);
+ 
+ ?>
---
+++ public_html/vita/actdat/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+
+    <link rel="stylesheet" type="text/css" href="../../style.css" media="screen">
+
+    <title>OurBrew</title>
+
+   </head>
+  <body>
+     <h1> Act.dat obtaining script </h1>
+     <form method="post" action="actdat.php">
+     <p>
+       <label for="loginid">E-Mail : </label> <input type="email" name="loginid" id="loginid" required autofocus />
+     </p>
+     <p>
+       <label for="password">Password : </label> <input type="password" name="password" id="password" required />
+     </p>
+     <p>
+       <label for="consoleid">IDPS : </label> <input type="text" name="consoleid" id="consoleid" required />
+   </p>
+     <input type="submit" value="Send" />
+     </form>
+     <br>
+     <p>
+     <a href="http://playstationhax.it/forums/topic/550-news-vita-hack/?page=4#comment-23519">Script from here</a>
+     </p>
+  </body>
+</html>
+
